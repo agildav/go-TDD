e2fa908ae5198e8c06a6cd026ada44b318387fab
Alberto Gil <agildav@gmail.com>
2019-03-31 16:14:08 -0400

    Added interfaces

--- Fundamentals/StructsMethodsInterfaces/smi.go
+++ Fundamentals/StructsMethodsInterfaces/smi.go
@@ -1,15 +1,37 @@
 package structsmethodsinterfaces
 
-func perimeter(width, height float64) float64 {
-	if width < 0 || height < 0 {
-		return 0
-	}
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	area() float64
 }
 
-func area(width, height float64) float64 {
-	if width < 0 || height < 0 {
-		return 0
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) perimeter() float64 {
+	if r.width < 0 || r.height < 0 {
+		return 0.0
 	}
-	return width * height
+	return 2 * (r.width + r.height)
+}
+
+func (r Rectangle) area() float64 {
+	if r.width < 0 || r.height < 0 {
+		return 0.0
+	}
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	if c.radius < 0 {
+		return 0.0
+	}
+	return math.Pi * c.radius * c.radius
 }
--- Fundamentals/StructsMethodsInterfaces/smi_test.go
+++ Fundamentals/StructsMethodsInterfaces/smi_test.go
@@ -21,7 +21,9 @@ func TestPerimeter(t *testing.T) {
 	t.Run("perimeter of a rectangle ", func(t *testing.T) {
 		width := 20.0
 		height := 10.0
-		got := perimeter(width, height)
+
+		rectangle := Rectangle{width, height}
+		got := rectangle.perimeter()
 		want := 60.0
 
 		checkPerimeter(t, got, want)
@@ -30,7 +32,9 @@ func TestPerimeter(t *testing.T) {
 	t.Run("negative values of a rectangle give zero perimeter", func(t *testing.T) {
 		width := (-20.0)
 		height := 10.0
-		got := perimeter(width, height)
+
+		rectangle := Rectangle{width, height}
+		got := rectangle.perimeter()
 		want := 0.0
 
 		checkPerimeter(t, got, want)
@@ -39,8 +43,9 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, got, want float64) {
+	checkArea := func(t *testing.T, s Shape, want float64) {
 		t.Helper()
+		got := s.area()
 
 		// Assert the message
 		if got != want {
@@ -50,21 +55,47 @@ func TestArea(t *testing.T) {
 		}
 	}
 
-	t.Run("area of a rectangle ", func(t *testing.T) {
+	// Rectangles
+
+	t.Run("area of a rectangle", func(t *testing.T) {
 		width := 20.0
 		height := 10.0
-		got := area(width, height)
+
+		rectangle := Rectangle{width, height}
+
 		want := 200.0
 
-		checkArea(t, got, want)
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("negative values of a rectangle give zero area", func(t *testing.T) {
 		width := (-20.0)
 		height := 10.0
-		got := area(width, height)
+
+		rectangle := Rectangle{width, height}
 		want := 0.0
 
-		checkArea(t, got, want)
+		checkArea(t, rectangle, want)
 	})
+
+	// Circles
+
+	t.Run("area of a circle", func(t *testing.T) {
+		radius := 10.0
+
+		circle := Circle{radius}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
+
+	t.Run("negative radius of a circle give zero area", func(t *testing.T) {
+		radius := (-10.0)
+
+		circle := Circle{radius}
+		want := 0.0
+
+		checkArea(t, circle, want)
+	})
+
 }
