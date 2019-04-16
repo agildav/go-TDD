78189cfb4432b9901931e723bce8be53e69a9a7d
Alberto Gil <agildav@gmail.com>
2019-04-15 21:39:42 -0400

    Added more tests

--- Fundamentals/Pointers/pointers.go
+++ Fundamentals/Pointers/pointers.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin float64
 
@@ -8,12 +11,28 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-// // // // // // // // // // // // // // // // // // // //
+type Stringer interface {
+	String() string
+}
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
-	if amount >= 0 {
-		w.balance -= amount
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+
+// Wallet Errors
+var errInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+var errNegativeWithdrawal = errors.New("cannot withdraw, negative amount")
+
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errInsufficientFunds
+	} else if amount <= 0 {
+		return errNegativeWithdrawal
 	}
+
+	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
@@ -26,11 +45,7 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-// // // // // // // // // // // // // // // // // // // //
-
-type Stringer interface {
-	String() string
-}
+// // // // // // // // // // // // // // // // // // // // // // // // // //
 
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%f BTC", b)
--- Fundamentals/Pointers/pointers_test.go
+++ Fundamentals/Pointers/pointers_test.go
@@ -6,19 +6,7 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, w Wallet, want Bitcoin) {
-		t.Helper()
-		got := w.Balance()
-
-		if got != want {
-			errMsg := fmt.Sprintf("got -> %s, want -> %s", got, want)
-			t.Errorf(colorError(errMsg))
-		}
-	}
-
 	// Deposit
-
 	t.Run("positive deposit", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(0.0)}
 		deposit := Bitcoin(10.0)
@@ -40,15 +28,51 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10.0)}
 		amount := Bitcoin(6.0)
 		want := Bitcoin(4.0)
-		wallet.Withdraw(amount)
+		err := wallet.Withdraw(amount)
+
 		assertBalance(t, wallet, want)
+		assertError(t, err, nil)
 	})
 
-	t.Run("negative withdraw", func(t *testing.T) {
+	t.Run("negative withdrawal", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10.0)}
 		amount := Bitcoin(-6.0)
 		want := Bitcoin(10.0)
-		wallet.Withdraw(amount)
+		err := wallet.Withdraw(amount)
+
 		assertBalance(t, wallet, want)
+		assertError(t, err, errNegativeWithdrawal)
 	})
+
+	t.Run("withdraw insufficient bitcoins", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10.0)}
+		amount := Bitcoin(16.0)
+		want := Bitcoin(10.0)
+		err := wallet.Withdraw(amount)
+
+		assertBalance(t, wallet, want)
+		assertError(t, err, errInsufficientFunds)
+	})
+}
+
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+
+// Helpers
+func assertBalance(t *testing.T, w Wallet, want Bitcoin) {
+	t.Helper()
+	got := w.Balance()
+
+	if got != want {
+		errMsg := fmt.Sprintf("got -> %s, want -> %s", got, want)
+		t.Errorf(colorError(errMsg))
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		errMsg := fmt.Sprintf("got -> %s, want -> %s", got, want)
+		t.Errorf(colorError(errMsg))
+	}
 }
